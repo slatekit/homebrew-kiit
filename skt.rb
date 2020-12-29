21c64478c9ebf08cbcf647954f79393a712058de
kishore <kishore.pr4@gmail.com>
2020-12-28 23:04:18 -0500

    FIX: Test hombrew with latest from git

--- skt.rb
+++ skt.rb
@@ -3,9 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Skt < Formula
   desc ""
-  homepage ""
-  url "https://github.com/code-helix/skt/archive/v1.29.0.tar.gz"
-  sha256 "dd4fe631a42185433b1a145f49438d31e9ba8c713d217dc3c6bb57420fafa361"
+  homepage "https://www.slatekit.com"
+  #url "https://github.com/code-helix/skt/archive/v1.29.0.tar.gz"
+  head 'https://github.com/code-helix/skt.git'
+  #sha256 "dd4fe631a42185433b1a145f49438d31e9ba8c713d217dc3c6bb57420fafa361"
   license "Apache-2.0"
 
   # depends_on "cmake" => :build
