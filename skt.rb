f52ed0f23e17c6c32bc700fafae9331860ef83be
kishore <kishore.pr4@gmail.com>
2020-12-30 02:14:31 -0500

    BUMP: 2.2.0

--- skt.rb
+++ skt.rb
@@ -5,8 +5,11 @@ class Skt < Formula
   desc ""
   homepage "https://www.slatekit.com"
   
-  url "https://github.com/code-helix/skt/archive/v2.1.0.tar.gz"
-  sha256 "eba56cfdf72e3ac65c368a0671d3edc69fbac3c5ac602170a2c2ffd516b748f1"
+  url "https://github.com/code-helix/skt/archive/v2.2.0.tar.gz"
+  sha256 "8f4caf0b44291dadd43e9d0770ae60fbd5b60aeec5c44fe70e452b95ac076d41"
+  
+  #url "https://github.com/code-helix/skt/archive/v2.1.0.tar.gz"
+  #sha256 "eba56cfdf72e3ac65c368a0671d3edc69fbac3c5ac602170a2c2ffd516b748f1"
   
   #url "https://github.com/code-helix/skt/archive/v1.29.0.tar.gz"
   #sha256 "dd4fe631a42185433b1a145f49438d31e9ba8c713d217dc3c6bb57420fafa361"
