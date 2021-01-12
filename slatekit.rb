ab95898fb38a0962870040e593313c00440aa589
kishore <kishore.pr4@gmail.com>
2021-01-11 22:53:52 -0500

    v1.34.4

--- slatekit.rb
+++ slatekit.rb
@@ -13,8 +13,8 @@ class Slatekit < Formula
   desc ""
   homepage "https://www.slatekit.com"
   
-  url "https://github.com/slatekit/slatekit-cli/archive/v1.34.2.tar.gz"
-  sha256 "dc5951cbbdac2e042f95b6f85fe407c7e17853dc636f1fe44f3fb294c34a7359"
+  url "https://github.com/slatekit/slatekit-cli/archive/v1.34.4.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "Apache-2.0"
 
   
