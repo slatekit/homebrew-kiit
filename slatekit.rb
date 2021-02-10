2fd272522fcf83fc998402b4984248b9f2fef731
kishore <kishore.pr4@gmail.com>
2021-02-09 21:02:07 -0500

    Patch: 2.1.4

--- slatekit.rb
+++ slatekit.rb
@@ -13,8 +13,8 @@ class Slatekit < Formula
   desc ""
   homepage "https://www.slatekit.com"
   
-  url "https://github.com/slatekit/slatekit-cli/archive/v2.1.3.tar.gz"
-  sha256 "01dfe9a24293ea82503d89142bf3ce9514932370bdacc425de6c90e50b43aa31"
+  url "https://github.com/slatekit/slatekit-cli/archive/v2.1.4.tar.gz"
+  sha256 "0d26f2a0e1dec12ec1554771c11a6c5b996a03c3129b08a713ba4d2a1c705780"
   license "Apache-2.0"
 
   
