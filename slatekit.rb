b93e7bcb1b515f3093f8d0eff05be7614c2893ef
kishore <kishore.pr4@gmail.com>
2022-12-11 20:06:05 -0500

    rename: using kiit

--- slatekit.rb
+++ slatekit.rb
@@ -9,11 +9,11 @@
 # license: refer to website and/or github
 # about: A Kotlin tool-kit for Server & Android
 #</slate_header>
-class Slatekit < Formula
+class Kiit < Formula
   desc ""
-  homepage "https://www.slatekit.com"
+  homepage "https://www.kiit.dev"
   
-  url "https://github.com/slatekit/slatekit-cli/archive/v2.4.6.tar.gz"
+  url "https://github.com/slatekit/kiit-cli/archive/v2.8.0.tar.gz"
   sha256 "74fb46af23735177779768859430867d378b78eadabec75e3de0bdd08cfe1e8f"
   license "Apache-2.0"
 
@@ -21,7 +21,7 @@ class Slatekit < Formula
   def install
     puts "prefix = #{prefix}"
     puts "buildpath = #{buildpath}"
-    puts "Beginning Slate Kit CLI installation"
+    puts "Beginning Kiit CLI installation"
     bin.install 'slatekit'
     prefix.install Dir["lib"]
     prefix.install Dir["conf"]
